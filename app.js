3888b89690d25c142fdb6ad54e850ac99bebe57f
repl.it user <replituser@example.com>
2020-07-21 17:53:23 +0000

    段階ごとにappを分割した

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!!!"));
 
-app.listen(8080, () => console.log("Example app listening on port 8080!"));
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
